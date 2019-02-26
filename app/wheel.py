8fa1493fbfbeecc61fcaf04fdd4256d5f41578b3
Jill San Luis <jsanluis@freshbooks.com>
2019-02-25 22:28:35 -0500

    Magic, wheel just became super slow and expensive.

--- app/wheel.py
+++ app/wheel.py
@@ -1,3 +1,6 @@
+from time import sleep
+
+
 class Wheel(object):
     def __init__(self, rim, tire):
         self.rim = rim
@@ -5,4 +8,5 @@ class Wheel(object):
 
     @property
     def diameter(self):
+        sleep(10)
         return self.rim + (self.tire * 2)
