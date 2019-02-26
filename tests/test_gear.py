c2931c614c5dfb7698c38bbd6644a5728d712392
Jill San Luis <jsanluis@freshbooks.com>
2019-02-25 23:30:30 -0500

    Testing outgoing commands

--- tests/test_gear.py
+++ tests/test_gear.py
@@ -1,3 +1,5 @@
+from unittest.mock import Mock
+
 from pytest import approx
 from app.gear import Gear
 
@@ -9,6 +11,17 @@ class DiameterDouble(object):
 
 
 class TestGear(object):
+    def setup(self):
+        self.observer = Mock()
+        self.gear = Gear(chainring=52, cog=11, wheel=DiameterDouble(), observer=self.observer)
+
     def test_calculates_gear_inches(self):
-        gear = Gear(chainring=52, cog=11, wheel=DiameterDouble())
-        assert 47.27 == approx(gear.gear_inches, 0.1)
+        assert 47.27 == approx(self.gear.gear_inches, 0.1)
+
+    def test_notifies_observers_when_cogs_change(self):
+        self.gear.set_cog(27)
+        self.observer.changed.assert_called_once_with(52, 27)
+
+    def test_notifies_observers_when_chainrings_change(self):
+        self.gear.set_chainring(42)
+        self.observer.changed.assert_called_once_with(42, 11)
