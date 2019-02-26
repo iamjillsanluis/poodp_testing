38ca62ac262220b832b69c381153454c2469b002
Jill San Luis <jsanluis@freshbooks.com>
2019-02-25 23:06:52 -0500

    Introduce observer dependency on Gear

--- app/gear.py
+++ app/gear.py
@@ -1,8 +1,9 @@
 class Gear(object):
-    def __init__(self, chainring, cog, wheel):
+    def __init__(self, chainring, cog, wheel, observer):
         self.chainring = chainring
         self.cog = cog
         self.wheel = wheel
+        self.observer = observer
 
     @property
     def gear_inches(self):
@@ -11,3 +12,14 @@ class Gear(object):
     @property
     def ratio(self):
         return self.chainring / self.cog
+
+    def set_cog(self, new_cog):
+        self.cog = new_cog
+        self._changed()
+
+    def set_chainring(self, new_chainring):
+        self.chainring = new_chainring
+        self._changed()
+
+    def _changed(self):
+        self.observer.changed(self.chainring, self.cog)
