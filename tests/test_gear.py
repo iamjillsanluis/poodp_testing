8346778fba1ecc1ae3e93a42020883ad68f150a2
Jill San Luis <jsanluis@freshbooks.com>
2019-02-25 22:26:18 -0500

    Fix the failing test due to floating point quirks.

--- tests/test_gear.py
+++ tests/test_gear.py
@@ -1,7 +1,8 @@
+from pytest import approx
 from app.gear import Gear
 
 
 class TestGear(object):
     def test_calculates_gear_inches(self):
         gear = Gear(chainring=52, cog=11, rim=26, tire=1.5)
-        assert 137.1 == gear.gear_inches
+        assert 137.1 == approx(gear.gear_inches, 0.1)
--- tests/test_wheel.py
+++ tests/test_wheel.py
@@ -1,7 +1,8 @@
+from pytest import approx
 from app.wheel import Wheel
 
 
 class TestWheel(object):
     def test_calculates_diameter(self):
         wheel = Wheel(26, 1.5)
-        assert 29 == wheel.diameter
+        assert 29 == approx(wheel.diameter, 0.01)
